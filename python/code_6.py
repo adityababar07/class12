d623c37080cabe81dea9506c87e892187f4c0760
hacker07 <hacker07@localhost.localdomain>
2021-01-14 22:17:13 +0530

    code_6

--- python/code_6.py
+++ python/code_6.py
@@ -1,7 +1,35 @@
+# write a program to display bar graph between person and score
 
-
+import numpy as np
 import matplotlib.pyplot as plt
-squares = [1, 4, 9, 16, 25]
-fig, ax = plt.subplots()
-ax.plot(squares)
+
+# data to plot
+
+n_groups = 4
+aditya = (90, 55, 40, 65)
+avinash = (85, 62, 54, 20)
+
+# create plot
+
+index = np.arange(n_groups)
+bar_width = 0.35
+opacity = 0.8
+
+rects1 = plt.bar(index, aditya, bar_width,
+alpha = opacity,
+color = 'b',
+label = 'Aditya')
+
+rects2 = plt.bar(index, avinash, bar_width,
+alpha = opacity,
+color = 'g',
+label = 'Avinash')
+
+plt.xlabel('Person')
+plt.ylabel('Scores')
+plt.title('Scores by person')
+plt.xticks(index + bar_width, ('A', 'B', 'C', 'D'))
+plt.legend()
+
+plt.tight_layout()
 plt.show()
