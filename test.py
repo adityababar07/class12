5aa2264b71bfb783a2285476f35f459c6123d2de
adityababar07 <adityababar715@gmail.com>
2021-02-27 16:19:53 +0530

    initial changes

---
+++ test.py
@@ -0,0 +1,16 @@
+def ChangeVal(M,N):
+    for i in range(4):
+        if M[i] == 0:
+            pass
+
+L = [25, 8, 75, 12]
+ChangeVal(L,4)
+for i in L:
+    print(i, end="#")
+
+# m = 3
+
+# if m == 3:
+#     print("okay")
+# elif m != 4:
+#     print("not okay")
